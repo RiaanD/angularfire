f5fdca09b5965f5627a7a5e2604cb85e103cdc94
Riaan De Nysschen <denysschenr@hq.za.ovationsgroup.com>
2017-03-13 12:22:01 +0200

    cleanup

--- app.js
+++ app.js
@@ -2,20 +2,24 @@
 
 var sampleApp = angular.module("sampleApp", ["firebase"]);
 
-sampleApp.controller("SampleCtrl",function SampleCtrl($scope, $firebaseObject) {
- 
-     var ref = firebase.database().ref('data');
+sampleApp.controller("SampleCtrl", function SampleCtrl($scope, $firebaseObject) {
+
+  var ref = firebase.database().ref('data');
   // download the data into a local object
-   $scope.data = $firebaseObject(ref);
+  $scope.data = $firebaseObject(ref);
   // putting a console.log here won't work, see below
 
-  
+
   $scope.data.message = "hello again Boss";
 
-  $scope.data.$save().then(function(ref) {
-  ref.key === $scope.data.$id; // true
-}, function(error) {
-  console.log("Error:", error);
+  $scope.data.$save().then(function (ref) {
+    ref.key === $scope.data.$id; // true
+  }, function (error) {
+    console.log("Error:", error);
+  });
+
 });
-  
-});
+
+
+
+
